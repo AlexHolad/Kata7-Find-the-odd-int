c0dffd514ec2a2e21569ace832e19eff7465c3fc
AlexHolad <alexej.holad@gmail.com>
2021-10-18 18:15:33 +0200

    solved

---
+++ kata.js
@@ -0,0 +1,23 @@
+
+const findOdd = (xs) => xs.reduce((a, b) => a ^ b);
+
+// const findOdd = (A) => {
+//   arr = A.sort();
+//   for (let i = 0; arr.length > 1; i++) {
+//     if (arr[0] === arr[1]) {
+//       arr.shift();
+//       arr.shift();
+//     } else {
+//       return arr[0];
+//     }
+//   }
+//   return arr[0];
+// };
+
+console.log(findOdd([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]));
+console.log(findOdd([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]));
+console.log(findOdd([1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5]));
+console.log(findOdd([20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5]));
+console.log(findOdd([10]));
+console.log(findOdd([1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1]));
+console.log(findOdd([5, 4, 3, 2, 1, 5, 4, 3, 2, 10, 10]));
